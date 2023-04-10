56d96447f7569241f6f1676ec60bb2ba83ad3877
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2023-04-10 14:23:34 +0100

    Update Data Base file

--- resources/memeCreator.sql
+++ resources/memeCreator.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 05-Mar-2022 às 12:45
+-- Tempo de geração: 10-Abr-2023 às 15:22
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -34,6 +34,21 @@ CREATE TABLE `meme` (
   `idUtilizador` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `meme`
+--
+
+INSERT INTO `meme` (`id`, `imagemMeme`, `idTemplate`, `idUtilizador`) VALUES
+(42, '/resources/imagesMemes/6210cf18c7c57-1645268760.png', 45, 3),
+(43, '/resources/imagesMemes/6210d032ecb9d-1645269042.png', 52, 2),
+(44, '/resources/imagesMemes/6210d16074ed4-1645269344.png', 49, 2),
+(45, '/resources/imagesMemes/6210d1e3aead1-1645269475.png', 51, 2),
+(46, '/resources/imagesMemes/6210d30e88134-1645269774.png', 51, 4),
+(47, '/resources/imagesMemes/6210d454d96c8-1645270100.png', 50, 4),
+(48, '/resources/imagesMemes/6210d4e3e844e-1645270243.png', 47, 4),
+(57, '/resources/imagesMemes/633c9a866457d-1664916102.png', 53, 1),
+(58, '/resources/imagesMemes/64308ce500867-1680903397.png', 60, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +64,23 @@ CREATE TABLE `template` (
   `ativo` tinyint(4) NOT NULL,
   `idUtilizador` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `template`
+--
+
+INSERT INTO `template` (`id`, `nome`, `imagem`, `width`, `height`, `ativo`, `idUtilizador`) VALUES
+(45, 'Drake Hotline Bling', '/resources/imagesTemplates/61fd326a0bc40-1643983466.jpg', 1200, 1200, 1, 3),
+(47, 'Distracted Boyfriend', '/resources/imagesTemplates/61fd3282e77b9-1643983490.jpg', 1200, 800, 1, 3),
+(49, 'Left Exit 12 Off Ramp', '/resources/imagesTemplates/61fd329ca0e03-1643983516.jpg', 804, 767, 1, 3),
+(50, 'UNO Draw 25 Cards', '/resources/imagesTemplates/6210cbd4ab877-1645267924.jpg', 500, 494, 1, 3),
+(51, 'Two Buttons', '/resources/imagesTemplates/6210cbea5f813-1645267946.jpg', 600, 908, 1, 3),
+(52, 'Running Away Balloon', '/resources/imagesTemplates/6210cc0f9bc5f-1645267983.jpg', 761, 1024, 1, 3),
+(53, 'Buff Doge VS Cheems', '/resources/imagesTemplates/6210cc3e22a9b-1645268030.png', 937, 720, 1, 3),
+(57, 'fhgsfdghdfg', '/resources/imagesTemplates/6220bb7a1bad8-1646312314.png', 450, 450, 0, 1),
+(58, 'gsgsdfb', '/resources/imagesTemplates/6220eb080b19c-1646324488.jpg', 500, 375, 1, 5),
+(59, 'Stackoverflow', '/resources/imagesTemplates/64308b46bd92d-1680902982.png', 1000, 1000, 0, 1),
+(60, 'Stackoverflow', '/resources/imagesTemplates/64308c0a89a12-1680903178.png', 607, 1000, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -67,7 +99,18 @@ CREATE TABLE `utilizador` (
   `online` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+--
+-- Extraindo dados da tabela `utilizador`
+--
+
+INSERT INTO `utilizador` (`id`, `primeiroNome`, `ultimoNome`, `email`, `imagem`, `password`, `admin`, `online`) VALUES
+(1, 'Frederico', 'Silva', 'fredericosilva2002@hotmail.com', '/resources/imagesPerfil/6373fc5adb775-1668545626.jpg', 'a5fd082755127b5daae6214a2f58e537', 1, 0),
+(2, 'Paulo', 'Sousa', 'paulosousa@gmail.com', '/resources/imagesPerfil/61f6d746c6f9b-1643566918.jpg', 'a5fd082755127b5daae6214a2f58e537', 0, 0),
+(3, 'Carolina', 'Duarte', 'carolinaduarte@gmail.com', '/resources/imagesPerfil/620d71dbbb569-1645048283.png', '95ceac3aaefd369c4afc8150d7f274be', 1, 0),
+(4, 'Fernando', 'Soares', 'fernandosoares@gmail.com', '/resources/imagesPerfil/noPhotoPerfil.png', '95ceac3aaefd369c4afc8150d7f274be', 0, 0),
+(5, 'José', 'Neves', 'joseneves@gmail.com', '/resources/imagesPerfil/6220ea7d5cf9a-1646324349.png', '95ceac3aaefd369c4afc8150d7f274be', 1, 0),
+(6, 'João', 'Miguel', 'joaomiquel@gmail.com', '/resources/images/imagesPerfil/noPhotoPerfil.png', '95ceac3aaefd369c4afc8150d7f274be', 0, 0),
+(7, 'Miguel', 'Soares', 'miguelsoares@gmail.com', '/resources/imagesPerfil/noPhotoPerfil.png', '95ceac3aaefd369c4afc8150d7f274be', 0, 0);
 
 --
 -- Índices para tabelas despejadas
@@ -102,13 +145,13 @@ ALTER TABLE `utilizador`
 -- AUTO_INCREMENT de tabela `meme`
 --
 ALTER TABLE `meme`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT de tabela `template`
 --
 ALTER TABLE `template`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de tabela `utilizador`
